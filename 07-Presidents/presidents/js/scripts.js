e3ff63781edc3925ca1f0355bad5677f5b6015ac
Ash Worthington <worthash2022@gmail.com>
2026-02-19 13:29:49 -0700

    add js to presidents project

--- 07-Presidents/presidents/js/scripts.js
+++ 07-Presidents/presidents/js/scripts.js
@@ -1,1 +1,21 @@
-console.log("I am connected");
+import {presidents} from '../data/presidents.js'
+const createCard = document.querySelector('#cards');
+const setLink = "//resources.dgmuvu.com/presidents/"
+
+for (let x = 0; x < presidents.length; x++) {
+    let presidentName = document.createElement('div')
+    presidentName.innerHTML = `<p>${presidents[x].name}</p>`
+    
+    let presidentImg = document.createElement('img')
+    presidentImg.setAttribute('src', setLink+presidents[x].photo)
+    presidentImg.setAttribute('alt', presidents[x].name)
+    let presidentCaption = document.createElement('figcaption')
+    presidentCaption.innerHTML = `Served from ${presidents[x].took_office} - ${presidents[x].left_office} <br> Lived from ${presidents[x].born} to ${presidents[x].died} <br> Party: ${presidents[x].party}`
+    let presidentFigure = document.createElement('figure')
+    presidentFigure.appendChild(presidentName)
+    presidentFigure.appendChild(presidentImg)
+    presidentFigure.appendChild(presidentCaption)
+    createCard.appendChild(presidentFigure)
+} //end of the for loop
+
+console.log(presidents)
