f98bbfa79ea1b0cfc3755878b59feb0c56d4dfd5
Ash Worthington <worthash2022@gmail.com>
2026-02-19 14:13:03 -0700

    add css to presidents project

--- 07-Presidents/presidents/js/scripts.js
+++ 07-Presidents/presidents/js/scripts.js
@@ -4,13 +4,13 @@ const setLink = "//resources.dgmuvu.com/presidents/"
 
 for (let x = 0; x < presidents.length; x++) {
     let presidentName = document.createElement('div')
-    presidentName.innerHTML = `<p>${presidents[x].name}</p>`
+    presidentName.innerHTML = `<h2>${presidents[x].name}</h2>`
     
     let presidentImg = document.createElement('img')
     presidentImg.setAttribute('src', setLink+presidents[x].photo)
     presidentImg.setAttribute('alt', presidents[x].name)
     let presidentCaption = document.createElement('figcaption')
-    presidentCaption.innerHTML = `Served from ${presidents[x].took_office} - ${presidents[x].left_office} <br> Lived from ${presidents[x].born} to ${presidents[x].died} <br> Party: ${presidents[x].party}`
+    presidentCaption.innerHTML = `Served from ${presidents[x].took_office} to ${presidents[x].left_office} <br> Lived from ${presidents[x].born} to ${presidents[x].died} <br> Party: ${presidents[x].party}`
     let presidentFigure = document.createElement('figure')
     presidentFigure.appendChild(presidentName)
     presidentFigure.appendChild(presidentImg)
