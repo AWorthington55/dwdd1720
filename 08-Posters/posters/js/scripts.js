8e8aeeab406672e0a57122f64196d808143828ba
Ash Worthington <worthash2022@gmail.com>
2026-03-03 10:33:18 -0700

    add css and js

--- 08-Posters/posters/js/scripts.js
+++ 08-Posters/posters/js/scripts.js
@@ -1,1 +1,6 @@
-console.log("I am connected");
+const redBtn = document.querySelector("#red")
+const greenBtn = document.querySelector("#green")
+const posterBody = document.querySelector("main")
+
+redBtn.addEventListener('click' , () => posterBody.className = "posterGrid")
+greenBtn.addEventListener('click' , () => posterBody.className = "posterList")
