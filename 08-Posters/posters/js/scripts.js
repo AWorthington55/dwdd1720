e14a9b8675ab877ee5fdfc752f1766870b10e842
Ash Worthington <worthash2022@gmail.com>
2026-03-04 10:54:29 -0700

    finish up posters project

--- 08-Posters/posters/js/scripts.js
+++ 08-Posters/posters/js/scripts.js
@@ -3,11 +3,13 @@ const newPoster = document.querySelector('.information')
 const posterLink = "//resources.dgmuvu.com/films/"
 
 for (let x = 0; x < films.length; x++) {
-    let posterInfo = document.createElement('section')
+    let posterInfo = document.createElement('h2')
     posterInfo.innerHTML = `<h2>${films[x].title}</h2>`
     let posterImg = document.createElement('img')
     posterImg.setAttribute('src', posterLink+films[x].url)
     posterImg.setAttribute('alt', films[x].title)
+    let infoDiv = document.createElement('div')
+    infoDiv.classList.add('info')
     let posterDirector = document.createElement('p')
     posterDirector.textContent = `Director: ${films[x].director}`
     let posterProducer = document.createElement('p')
@@ -17,9 +19,10 @@ for (let x = 0; x < films.length; x++) {
     let posterFigure = document.createElement('figure')
     posterFigure.appendChild(posterInfo)
     posterFigure.appendChild(posterImg)
-    posterFigure.appendChild(posterDirector)
-    posterFigure.appendChild(posterProducer)
-    posterFigure.appendChild(posterRelease)
+    infoDiv.appendChild(posterDirector)
+    infoDiv.appendChild(posterProducer)
+    infoDiv.appendChild(posterRelease)
+    posterFigure.appendChild(infoDiv)
     newPoster.appendChild(posterFigure)
 } // end of for loop
 
