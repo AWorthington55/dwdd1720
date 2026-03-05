1f5844cbf44885ff99b6b6ea354e03ad6f94de3a
Ash Worthington <worthash2022@gmail.com>
2026-03-05 10:34:46 -0700

    update js

--- 09-People/people/js/scripts.js
+++ 09-People/people/js/scripts.js
@@ -1,2 +1,18 @@
 import {people} from '../data/people.js'
 console.log(people)
+const navigation = document.querySelector('nav')
+const peopleInfo = document.querySelector('#info')
+function showPeople (x) {
+    x.forEach(person => {
+    const personFigure = document.createElement('figure')
+    const personImg = document.createElement('img')
+    const personCaption = document.createElement('figcaption')
+    personCaption.textContent = person.name
+    personFigure.appendChild(personImg)
+    personFigure.appendChild(personCaption)
+    peopleInfo.appendChild(personFigure)
+    }
+    )
+}
+
+showPeople(people)
