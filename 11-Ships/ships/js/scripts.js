ef3745aa7920c0dd04a8aefe3572405b85be67c8
Ash Worthington <worthash2022@gmail.com>
2026-03-23 13:20:13 -0600

    finish js for ships project

--- 11-Ships/ships/js/scripts.js
+++ 11-Ships/ships/js/scripts.js
@@ -1,1 +1,45 @@
-console.log("I am connected");
+//grab the data and wait for results
+fetch("data/starships.json")
+    .then((response) => response.json())
+    .then((shipArray) => {
+        createNav(shipArray)
+    })
+
+//populate navigation bar
+function createNav(ships) {
+    console.log(ships)
+    ships.forEach(ship => {
+        let shipBtn = document.createElement('button')
+        shipBtn.textContent = ship.name
+        shipBtn.addEventListener('click', () => shipShow(ship))
+        navigation.appendChild(shipBtn)
+    })//end of for each loop
+}
+
+//showing the ships
+
+function shipShow(shipData) {
+    let shipFigure = document.createElement('figure')
+    let shipImg = document.createElement('img')
+    let shipCaption = document.createElement('figcaption')
+    console.log(shipData.url)
+    let urlArray = shipData.url.split('/')
+    console.log(urlArray[5])
+    shipImg.src = `https://resources.dgmuvu.com/ships/${urlArray[5]}.jpg`
+    shipImg.addEventListener('error', () => {
+        shipImg.src = "https://resources.dgmuvu.com/ships/placeholder.jpg"
+        shipCaption.textContent = `Unfortunately, the ${shipData.name} got blown up by the Death Star =(`
+    })
+
+    shipCaption.textContent = shipData.name
+    shipFigure.appendChild(shipImg)
+    shipFigure.appendChild(shipCaption)
+    view.textContent = " "
+    view.appendChild(shipFigure)
+    
+    
+}
+
+//add links to html
+const navigation = document.querySelector('nav')
+const view = document.querySelector('main')
