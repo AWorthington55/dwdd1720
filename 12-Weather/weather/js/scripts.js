5b1a302ba32c7b16ad7df9184d2ee641d721ad9e
Ash Worthington <worthash2022@gmail.com>
2026-03-25 10:35:29 -0600

    add API to js

--- 12-Weather/weather/js/scripts.js
+++ 12-Weather/weather/js/scripts.js
@@ -1,1 +1,18 @@
-console.log("I am connected");
+//write the default zipcode
+let zip = "84062"
+
+//set the path to the API for the weather
+const myKey = "9bbf76d4e9e7288dc6482d0d1f5797bc"
+const myAPI = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${myKey}&units=imperial`
+
+//fetch remote JSON data for current weather
+fetch(myAPI)
+    .then((response) => response.json())
+    .then((weatherData) => {
+        console.log(weatherData)
+    })
+//function for displaying the current weather
+
+//ask for new zipcode
+
+//validating data for the zipcode
