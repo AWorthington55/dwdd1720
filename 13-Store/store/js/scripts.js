8a0969493784f4574293f928368741aeb8fbebf1
Ash Worthington <worthash2022@gmail.com>
2026-03-30 15:19:41 -0600

    update js

--- 13-Store/store/js/scripts.js
+++ 13-Store/store/js/scripts.js
@@ -1,1 +1,27 @@
-console.log("I am connected");
+import {friends} from "../friend-JSON/friends.js"
+
+//grab HTML
+const storeNav = document.querySelector('nav')
+const storeView = document.querySelector('#view')
+
+//set up navigation
+friends.forEach(friend => {
+    console.log(friend)
+    const storeBtn = document.createElement('button')
+    storeBtn.textContent = `${friend.first} ${friend.last}`
+    storeBtn.addEventListener('click', () => showFriend(friend))
+    storeNav.appendChild(storeBtn)
+})
+
+
+function showFriend(friend) {
+    console.log(friend)
+
+    //create HTMl elements
+    let storeSection = document.createElement('section')
+    let storeName = document.createElement('h2')
+    let storePhoto = document.createElement('img')
+    let storeBirthday = document.createElement('p')
+    let storeEmployment = document.createElement('p')
+    let storeHobbies = document.createElement('ul')
+}//end function
